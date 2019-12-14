d935e6f2aa2d4a6f51583b3a12cf6eabc64e7fcf
formationarduinopython <formationarduinopython@gmail.com>
2019-12-14 11:59:27 +0100

    relecture condo microbit

--- condensateur-microbit/mb-code6-correction.py
+++ condensateur-microbit/mb-code6-correction.py
@@ -3,9 +3,14 @@ from microbit import *
 # on initialise la liaison uart via USB (tx et rx à rien)
 uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
 
+# décharge du condensateur
+pin8.write_digital(0)
+sleep(300)
+
 while True : 
-    if button_a.is_pressed(): 
-        t0 = running_time()
+    if button_a.is_pressed():
+        pin8.write_digital(1) # on attaque la charge
+        t0 = running_time() # initialisation du temps
         while True:  # boucle de l'acquisition ; c'est le fichier Python qui décide combien de temps elle va durer
             t = running_time()-t0
             ana = pin0.read_analog()
--- condensateur/determination-tau-polyfit.py
+++ condensateur/determination-tau-polyfit.py
@@ -1,6 +1,6 @@
-# la courbe théorique est charge = charge_max*(1-exp(-temps/tau))
-# soit en linéarisant : ln(1-charge/charge_max) = -temps/tau
-Y = np.log(1-charge/charge_max)
+# la courbe théorique est charge = ChargeMax*(1-exp(-temps/tau))
+# soit en linéarisant : ln(1-charge/ChargeMax) = -temps/tau
+Y = np.log(1-charge/ChargeMax)
 coeffs = np.polyfit(temps, Y, 1)
 a = coeffs[0]
 b = coeffs[1]
